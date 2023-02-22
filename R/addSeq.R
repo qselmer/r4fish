c595ed2345580f845a7226aa860cc833e9a28072
Elmer O. Quispe Salazar <qselmer@gmail.com>
2023-02-22 15:12:01 -0500

    funs

--- R/addSeq.R
+++ R/addSeq.R
@@ -8,10 +8,12 @@
 #'
 #' @examples
 #'
-addSeq <- function(sec, add = 2){
+addSeq <- function(sec, add = c(2, 0)){
   rz <- diff(sec)[1]
-  i1 <- rev(seq(range(sec)[1]-rz, by = -1*rz, length.out = add))
-  i2 <- seq(range(sec)[2]+rz, by =  1*rz, length.out = add)
+  if(length(add) == 1){add = rep(add, 2)}
+  i1 <- rev(seq(range(sec)[1]-rz, by = -1*rz, length.out = add[1]))
+  i2 <- seq(range(sec)[2]+rz, by =  1*rz, length.out = add[2])
   sec2 <- c(i1, sec,i2)
   return(sec2)
 }
+
--- check.R
+++ check.R
@@ -1,17 +1,12 @@
 rm(list = ls())
 #  -----------------------------------------------------------------------
-
 require(devtools)
 session_info()
 
 getwd()
 build()
-
-# load(file = "data/species.RData")
-# str(species)
-
 install(reload = T, force = T)
 document()
-
 check()
+
 
