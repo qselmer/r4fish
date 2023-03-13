d0d4da72c601da21a414c475779e30169788265d
Elmer O. Quispe Salazar <qselmer@gmail.com>
2023-03-13 11:07:59 -0500

    Update validate_data.R

--- R/validate_data.R
+++ R/validate_data.R
@@ -1,10 +1,10 @@
 
-validate_data <- function(data = data, 
+validate_data <- function(data = data,
                           sp = "anchoveta",
-                          output_dir = "outputs", 
+                          output_dir = "outputs",
                           output_file = "Information.docx"){
-  
-  rmarkdown :: render(input = "R/InformationVesselSet.Rmd",
+
+  rmarkdown :: render(input = "InformationVesselSet.Rmd",
                       output_dir = output_dir,
                       output_file = output_file)
   return(invisible())
