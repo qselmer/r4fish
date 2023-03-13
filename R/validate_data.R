071cfe0cf8ddec88f35d3e7d2a82817bf041684a
Elmer O. Quispe Salazar <qselmer@gmail.com>
2023-03-13 11:02:02 -0500

    word_document

---
+++ R/validate_data.R
@@ -0,0 +1,16 @@
+
+validate_data <- function(data = data, 
+                          sp = "anchoveta",
+                          output_dir = "outputs", 
+                          output_file = "Information.docx"){
+  
+  rmarkdown :: render(input = "R/InformationVesselSet.Rmd",
+                      output_dir = output_dir,
+                      output_file = output_file)
+  return(invisible())
+}
+
+
+
+
+
