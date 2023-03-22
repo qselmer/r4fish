0c0c184ad1a8c3206b648c0a0a58abd1c9e8c0c1
Elmer O. Quispe Salazar <qselmer@gmail.com>
2023-03-22 08:39:59 -0500

    upDate

--- R/validateSp.R
+++ R/validateSp.R
@@ -18,7 +18,7 @@ validateSp <- function(data = data,
                        sp = "anchoveta",
                        stock = NA,
                        cout = "outputs",
-                       file = "validateSp.pdf"){
+                       file = "validateSp.docx"){
   require(pander)
   require(sp)
   require(r4fish)
