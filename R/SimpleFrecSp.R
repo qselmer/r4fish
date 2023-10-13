05d5774f6af83d6247dc9927702ba9029da0b260
unknown <dinamica@dinamica-0254.imarpe.gob.pe>
2023-10-13 18:02:44 -0500

    pocos cambios

--- R/SimpleFrecSp.R
+++ R/SimpleFrecSp.R
@@ -3,7 +3,7 @@
 #'
 #' @param data datos del crucero en formato MF
 #' @param sp especie objetivo de la evaluación
-#' @param stock  stock objetivo de la evaluación
+#' @param stock stock objetivo de la evaluación
 #' @param save valor logico; ¿Se deben guardar la matriz de lances en "csv"?
 #'
 #' @return
@@ -11,31 +11,27 @@
 #'
 #' @examples
 #'
-SimpleFrecSp <- function(data = tmp,
+SimpleFrecSp <- function(data = x,
                          sp = "anchoveta",
                          stock = "nc",
                          save = T){
 
-  require(ruisu)
-
-# -------------------------------------------------------------------------
-
-name = trimws(data[1, "crucero"])
+name <- trimws(data[1, "crucero"])
 
 if(is.na(stock)){
   file <- paste("SimpleFrecSets", name, sp, sep = "_")
-  }else{
-    file <- paste("SimpleFrecSets", name, sp, stock, sep = "_")
-    }
+}else{
+  file <- paste("SimpleFrecSets", name, sp, stock, sep = "_")
+}
 
-name = file.path(cout, file)
+name <- file.path(cout, file)
 
-marks0 <- r4fish:::.getMarks(sp = sp, stock = stock)
-rmrk <- range(marks0)
-rbin <- r4fish:::.getSp(sp = sp, stock = stock)$lengthBin
+marks0    <- r4fish:::.getMarks(sp = sp, stock = stock)
+rmrk      <- range(marks0)
+rbin      <- r4fish:::.getSp(sp = sp, stock = stock)$lengthBin
 range_lat <- r4fish:::.getLati(sp = sp, stock = stock)
-juv0 <- r4fish:::.getJuv(sp = sp, stock = stock)
-marks <- data.frame(l = marks0)
+juv0      <- r4fish:::.getJuv(sp = sp, stock = stock)
+marks     <- data.frame(l = marks0)
 
 data[, "sp"] <- tolower(data[, "sp"])
 
@@ -47,32 +43,32 @@ data <- data[!is.na(data$frec), ]
 data <- data[!is.na(data$lat), ]
 data <- data[!is.na(data$lon), ]
 
-data$vessel_set = paste(data$buque, data$lance, sep = "-")
-listVessel <- unique(data$vessel_set)
-vectorRes <- NULL; vectorSet <- NULL
+data$vessel_set <- paste(data$buque, data$lance, sep = "-")
+listVessel      <- unique(data$vessel_set)
+vectorRes       <- NULL; vectorSet <- NULL
 
 for(r in seq_along(listVessel)){
   tmp <- data[data$vessel_set  == listVessel[r],]
   tmp <- tmp[,c("crucero", "buque","lance","lon","lat",
                 "l","frec", "captura")]
   vectorLance <- c(tmp$crucero[1], tmp$buque[1], tmp$lance[1])
-  distC <-  distCoast(lon = tmp$lon[1], lat = tmp$lat[1])
-  vectorCoor <- c(tmp$captura[1],tmp$lon[1],tmp$lat[1], distC)
-  tmp <- merge(tmp, marks, all = T)
+  distC       <- distCoast(lon = tmp$lon[1], lat = tmp$lat[1])
+  vectorCoor  <- c(tmp$captura[1],tmp$lon[1],tmp$lat[1], distC)
+  tmp         <- merge(tmp, marks, all = T)
   tmp$frec[is.na(tmp$frec)] <- 0
-  vectorFreq <- t(tmp$frec)
-  vectorOut <- c(vectorCoor, vectorFreq)
-  vectorRes <- rbind(vectorRes, vectorOut)
-  vectorSet <- rbind(vectorSet,vectorLance)
+  vectorFreq  <- t(tmp$frec)
+  vectorOut   <- c(vectorCoor, vectorFreq)
+  vectorRes   <- rbind(vectorRes, vectorOut)
+  vectorSet   <- rbind(vectorSet,vectorLance)
 }
 
 vectorRes <- as.data.frame.array(vectorRes)
 vectorSet <- as.data.frame.array(vectorSet)
-tmp.out <- cbind(vectorSet, vectorRes)
+tmp.out   <- cbind(vectorSet, vectorRes)
 
 colnames(tmp.out) <- c("crucero", "buque","lance", "captura", "lon", "lat", "dc", marks$l)
 rownames(tmp.out) <- NULL
-tmp.out <- tmp.out[order(tmp.out$lat, decreasing = T),]
+tmp.out           <- tmp.out[order(tmp.out$lat, decreasing = T),]
 
 if(save == T){
   nameFile <- paste0(name, ".csv")
