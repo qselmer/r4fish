dcc49452132f192d83cb3f2511bd04107194c46d
Elmer Ovido Quispe Salazar <qselmer@gmail.com>
2024-01-29 02:21:35 -0500

    update

--- R/funcxJA.R
+++ R/funcxJA.R
@@ -1,11 +1,25 @@
 # -------------------------------------------------------------------------
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
 script_wd <- function(){
   dir <- dirname(rstudioapi::getSourceEditorContext()$path)
   setwd(dir)
   getwd()
-} 
+}
 
 # -------------------------------------------------------------------------
+#' Title
+#'
+#' @param file
+#'
+#' @return
+#' @export frequency_matrix
+#'
+#' @examples
 read_freq_F1 <- function(file){
   require(readxl)
   data <- read_excel(path = file, sheet = 1, skip = 3)
@@ -22,9 +36,26 @@ read_freq_F1 <- function(file){
 
 
 # function n°1:-------------------------------------------------------------------------
-plot.frequency_matrix <- function(x, 
-                                  relative = T, 
-                                  clean = T, 
+#' Title
+#'
+#' @param x
+#' @param relative
+#' @param clean
+#' @param ylim
+#' @param yinter
+#' @param juvMarks
+#' @param plotCol
+#' @param juvCol
+#' @param textx
+#' @param type
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot.frequency_matrix <- function(x,
+                                  relative = T,
+                                  clean = T,
                                   ylim = c(0, 20),
                                   yinter = 5,
                                   juvMarks = 51,
@@ -34,42 +65,42 @@ plot.frequency_matrix <- function(x,
                                   type = "barplot" #barplot
 ){
   require(r4fish)
-  
-  frec <- x 
+
+  frec <- x
   if (isTRUE(relative)) {frec <- apply(frec, MARGIN = 2, FUN = normalize)}
   base <- frec
   ncols <- ncol(base)
   marks <- as.numeric(rownames(base))
-  
+
   par(mfrow = c(ncols, 1), oma = c(5,7,2,7), mar = c(0,0,0,0))
-  
+
   for (p in 1:ncols) {
-    
+
     fig <- base[,p]
-    
+
     if (type == "lines") {
       if (isTRUE(clean)) {fig <- cleanZeros(fig)}; fig <- as.vector(fig)
-      plot(marks, fig, xlab = "", ylab = "", type = "l", lwd = 2, 
-           xaxt = "n", yaxt = "n", ylim = ylim, col = plotCol, 
+      plot(marks, fig, xlab = "", ylab = "", type = "l", lwd = 2,
+           xaxt = "n", yaxt = "n", ylim = ylim, col = plotCol,
            xaxs = "i",yaxs = "i")
       abline(v = juvMarks, lty = 3, col = juvCol, lwd = 2)
     }
-    
+
     if (type == "barplot") {
-      
+
       density <- ifelse(marks < juvMarks, 40, 100)
-      
-      bp <- barplot(height = fig, space = 0,  xlab = "", ylab = "", xaxt = "n", yaxt = "n", 
-                    ylim = ylim, col = plotCol, border = "gray10", xaxs = "i",yaxs = "i", 
+
+      bp <- barplot(height = fig, space = 0,  xlab = "", ylab = "", xaxt = "n", yaxt = "n",
+                    ylim = ylim, col = plotCol, border = "gray10", xaxs = "i",yaxs = "i",
                     density = density); box()
     }
-    
+
     if (isTRUE(relative)) {
       juv <- sum(fig[marks < juvMarks], na.rm = T)
     }else{
       juv <- (sum(fig[marks < juvMarks], na.rm = T)/sum(fig, na.rm = T))*100
     }
-    
+
     if (p %% 2 == 0) {aty = 4}else{aty = 2}
     axis(aty, at = seq(ylim[1],ylim[2], yinter), labels = seq(ylim[1],ylim[2], yinter),
          las = 2, cex.axis = 1.5)
@@ -82,13 +113,13 @@ plot.frequency_matrix <- function(x,
       if (type == "barplot") {
         axis(1, at = bp, labels =  marks,  cex.axis = 1.5)
       }
-      
+
       mtext(text =  textx, side = 1, line = 3.5, outer = TRUE, cex = 1.5)
       mtext(text =  ifelse(isTRUE(relative), "Frecuencia relativa (%)", "Frecuencia"),
             side = 2, line = 4, outer = TRUE, cex = 1.5)
     }
-    
+
     mtext(text = paste("juv =",round(juv,0), "%"),side = 3, adj = 0.02, line = -2, cex = 1.5)
     mtext(text = colnames(base)[p] ,side = 3, adj = 0.97, line = -2, cex = 1.5)
   }
-}  
+}
